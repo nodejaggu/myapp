49b48b9aa07a7542975422efd9eea707faf7810c
Jagapathi Babu <jagapathi.rgukt@gmail.com>
2019-09-23 19:16:35 +0530

    changed to development

--- sw.js
+++ sw.js
@@ -1,2 +1,4 @@
 var cid = location.search.substring(5);
-importScripts("https://cdn-jp.gsecondscreen.com/static/webpushsw.js?cid="+cid);
+importScripts("http://localhost:8090/static_old/js/webpush/service_worker.js?cid="+cid);
+
+
